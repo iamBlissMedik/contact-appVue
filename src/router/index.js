68bd5b3f51cd974ad9461e85b9301aaee50b9b9d
Ellams Bliss <ellamsbliss@gmail.com>
2023-03-15 12:48:42 +1100

    setting up '/contacts' routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import ContactsView from "../views/ContactsView.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+    },
+    {
+      path: "/contacts",
+      name: "contacts",
+      component: ContactsView,
     },
   ],
 });
---
+++ src/views/ContactsView.vue
@@ -0,0 +1,10 @@
+        <script setup>
+        
+        </script>
+<template>
+    <div>
+        <h1>CONTACT</h1>
+    </div>
+</template>
+
+
