98e92c80ac49c325e775cd7bf77191bd77364319
Ellams Bliss <ellamsbliss@gmail.com>
2023-05-21 19:55:14 +1000

    null

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,11 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    {
       path: "/contacts",
       component: ContactsView,
       children: [
@@ -23,11 +28,6 @@ const router = createRouter({
           component: EditContactView,
         },
       ],
-    },
-    {
-      path: "/",
-      name: "home",
-      component: HomeView,
     },
     {
       path: "/messages",
