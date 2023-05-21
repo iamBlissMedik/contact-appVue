26d97bd8a2624748e5ed2e6d4177747fdc2b2c74
Ellams Bliss <ellamsbliss@gmail.com>
2023-05-21 20:08:24 +1000

    button in homepage to navigate to the contacts page

--- src/stores/Buttons.js
+++ src/stores/Buttons.js
@@ -1,24 +1,28 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
 export const useButtonsStore = defineStore("buttons", () => {
-const buttons = ref({
-  submit: {
-    type: "submit",
-    buttonName: "add contact",
-    class: "bg-blue-700 hover:bg-green-600 ",
-  },
-  cancel: {
-    type: "reset",
-    buttonName: "cancel",
-    class: "bg-gray-400 w-fit hover:bg-red-700",
-  },
-  edit: {
-    type: "submit",
-    buttonName: "save",
-    class: "bg-blue-700 hover:bg-green-600 ",
-  },
+  const buttons = ref({
+    submit: {
+      type: "submit",
+      buttonName: "add contact",
+      class: "bg-blue-700 hover:bg-green-600 ",
+    },
+    cancel: {
+      type: "reset",
+      buttonName: "cancel",
+      class: "bg-gray-400 w-fit hover:bg-red-700",
+    },
+    edit: {
+      type: "submit",
+      buttonName: "save",
+      class: "bg-blue-700 hover:bg-green-600 ",
+    },
+    contact: {
+      buttonName: "go to contacts",
+      class: "bg-blue-700 hover:bg-green-600 ",
+    },
+  });
+  return {
+    buttons,
+  };
 });
-    return {
-        buttons
-    }
-})
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,21 @@
+<script setup>
+import { useRouter } from "vue-router";
+import Button from "../components/Button.vue";
+import { useButtonsStore } from "../stores/Buttons";
+const store = useButtonsStore();
+const router = useRouter();
+const onClick = () => {
+  router.push("/contacts");
+};
+</script>
+
 <template>
-  <div class="text-9xl text-center">HOME</div>
+  <div class="flex flex-col items-center text-center gap-5">
+    <h1 class="text-9xl text-center">HOME</h1>
+    <Button
+      :class="store.buttons.contact.class"
+      :buttonName="store.buttons.contact.buttonName"
+      @click="onClick"
+    />
+  </div>
 </template>
