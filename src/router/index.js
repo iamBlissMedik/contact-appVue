815b817f41f69e94bd784b3e982d1b1b8f7d7750
Ellams Bliss <ellamsbliss@gmail.com>
2023-05-21 19:50:55 +1000

    make contact page the default page

--- src/router/index.js
+++ src/router/index.js
@@ -9,11 +9,6 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      name: "home",
-      component: HomeView,
-    },
-    {
       path: "/contacts",
       component: ContactsView,
       children: [
@@ -28,6 +23,11 @@ const router = createRouter({
           component: EditContactView,
         },
       ],
+    },
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
       path: "/messages",
